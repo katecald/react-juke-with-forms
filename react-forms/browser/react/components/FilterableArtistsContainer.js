cc187f0b8909a9dba2315290f1c00093b90b37a8
Kate Caldwell <kate.kec.caldwell@gmail.com>
2017-03-29 18:34:35 -0400

    Filter components added

---
+++ react-forms/browser/react/components/FilterableArtistsContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Artists from './Artists';
+import FilterInput from './FilterInput';
+
+export default class FilterableArtistsContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filterValue: ''
+        }
+        this.setFilter = this.setFilter.bind(this);
+    }
+
+    setFilter(e) {
+        this.setState({filterValue: e.target.value})
+    }
+
+    render () {
+        let artists = this.props.artists;
+        let filteredArtists = artists.filter(artist => artist.includes(this.state.filterValue))
+
+        return (
+        <div>
+            <FilterInput setFilter={this.setFilter}/>
+            <Artists artists={filteredArtists} />
+        </div>
+        )
+    }
+}
